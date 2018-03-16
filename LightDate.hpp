21d44b07f5c6eb92f7ab5e9a41ccd50a48a6c7a4
Martynas Janonis <martynas.janonis.17@ucl.ac.uk>
2018-03-16 23:08:50 +0000

    Reformatted code

--- LightDate.hpp
+++ LightDate.hpp
@@ -160,19 +160,28 @@ bool Date::validDate(int y, Month m, int d)
   return false;
 }
 
-void Date::setYear(int y){
-  if(validDate(y,month,day)){year = y;}
-  else{throw std::runtime_error("Invalid year set");}
+void Date::setYear(int y)
+{
+  if (validDate(y, month, day)) { year = y; }
+  else {
+    throw std::runtime_error("Invalid year set");
+  }
 }
 
-void Date::setMonth(Month m){
-  if(validDate(year,m,day)){month = m;}
-  else{throw std::runtime_error("Invalid month set");}
+void Date::setMonth(Month m)
+{
+  if (validDate(year, m, day)) { month = m; }
+  else {
+    throw std::runtime_error("Invalid month set");
+  }
 }
 
-void Date::setDay(int d){
-  if(validDate(year,month,d)){day = d;}
-  else{throw std::runtime_error("Invalid day set");}
+void Date::setDay(int d)
+{
+  if (validDate(year, month, d)) { day = d; }
+  else {
+    throw std::runtime_error("Invalid day set");
+  }
 }
 
 #endif
